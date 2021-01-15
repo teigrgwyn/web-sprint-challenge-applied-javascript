81f74355562f49a2c309b08e8b630baff879dd56
Cory Young <teigrgwyn@gmail.com>
2021-01-15 12:49:37 -0600

    task 6, final commit

--- src/components/card.js
+++ src/components/card.js
@@ -1,25 +1,6 @@
 import axios from 'axios';
 
 const Card = (article) => {
-  // TASK 5
-  // ---------------------
-  // Implement this function, which should return the markup you see below.
-  // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
-  //
-  // <div class="card">
-  //   <div class="headline">{ headline }</div>
-  //   <div class="author">
-  //     <div class="img-container">
-  //       <img src={ authorPhoto }>
-  //     </div>
-  //     <span>By { authorName }</span>
-  //   </div>
-  // </div>
-  //
-
   // top-level div
   const div = document.createElement('div');
   div.classList.add("card");
@@ -47,41 +28,36 @@ const Card = (article) => {
   div4.appendChild(img);
   div3.appendChild(span);
 
+  console.log(div);
   return div;
 }
 
 const cardAppender = (selector) => {
-  // TASK 6
-  // ---------------------
-  // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `https://lambda-times-api.herokuapp.com/articles`
-  // However, the articles do not come organized in a single, neat array. Inspect the response closely!
-  // Create a card from each and every article object in the response, using the Card component.
-  // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
   axios.get('https://lambda-times-api.herokuapp.com/articles')
     .then(res => {
-      const DOM = document.querySelector(selector);
-      const newArray = Array.from(res.data);
-      const newArray2 = Array.from(res.data.articles);
-      console.log(newArray);
-      console.log(newArray2);
+      /*
+        - couldn't get this to work with recursion for the life of me
+        - if i could call res.data.articles.${topic}.forEach(), it'd be fine, but not sure how to do that
+      */
+      res.data.articles.bootstrap.forEach(article => {
+        document.querySelector(selector).appendChild(Card(article));
+      });
+      res.data.articles.javascript.forEach(article => {
+        document.querySelector(selector).appendChild(Card(article));
+      });
+      res.data.articles.jquery.forEach(article => {
+        document.querySelector(selector).appendChild(Card(article));
+      });
+      res.data.articles.node.forEach(article => {
+        document.querySelector(selector).appendChild(Card(article));
+      });
+      res.data.articles.technology.forEach(article => {
+        document.querySelector(selector).appendChild(Card(article));
+      });
     })
-   
-    // .then(res => {
-    //   const arrTopics = Object.keys(res.data.articles); // hack to get this to function correctly
-    //   console.log(arrTopics);
-    //   console.log(res.data.articles);
-
-    //   arrTopics.forEach(topic => {
-    //     res.data.articles.topic.forEach(article => {
-    //       document.querySelector(selector).appendChild(Card(item));
-    //     })
-    //   })
-    // })
-    // .catch(err => {
-    //   console.log(err);
-    // })
+    .catch(err => {
+      console.log(err);
+    })
 }
 
 export { Card, cardAppender }
